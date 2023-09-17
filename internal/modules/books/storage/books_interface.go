31f0c1ce8f0c2dcca34bdbb72cc8424ccfc884d7
paul1k <timofeev.pavel.art@gmail.com>
2023-09-17 14:59:51 +0300

    add mock storage && import gomock

--- internal/modules/books/storage/books_interface.go
+++ internal/modules/books/storage/books_interface.go
@@ -2,6 +2,8 @@ package storage
 
 import "github.com/Paul1k96/bookstorage/internal/models"
 
+//go:generate mockgen -source books_interface.go -destination=mocks/book_mock.go
+
 type BookStorager interface {
 	GetBooksByAuthor(author string) ([]models.Book, error)
 	GetAuthorsByBook(book string) ([]models.Author, error)
